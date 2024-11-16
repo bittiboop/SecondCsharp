eba1212c136425760ac67b2a4c70971e6b4d54f7
bittiboop <sstrelkova15@gmail.com>
2024-11-16 13:23:20 +0200

    added ex1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,48 @@
         {
             try
             {
+                var random = new Random(DateTime.Now.Millisecond);
+                Console.WriteLine("Enter the lower bound for the random number: ");
+                var lowerMaxRandomBound = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the upper bound for the random number: ");
+                var upperMaxRandomBound = Convert.ToInt32(Console.ReadLine());
+                if (lowerMaxRandomBound > upperMaxRandomBound)
+                {
+                    (lowerMaxRandomBound, upperMaxRandomBound) = (upperMaxRandomBound, lowerMaxRandomBound);
+                }
+                Console.WriteLine("Enter the size of the array: ");
+                var size = Convert.ToInt32(Console.ReadLine());
+                var array = new int[size];
+                for (var i = 0; i < size; i++)
+                {
+                    array[i] = random.Next(lowerMaxRandomBound, upperMaxRandomBound);
+                }
+                Console.WriteLine("Array: ");
+                foreach (var element in array)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.WriteLine();
 
+                Console.WriteLine("Odd elements from array: ");
+                foreach (var element in array)
+                {
+                    if (element % 2 != 0)
+                    {
+                        Console.Write(element + " ");
+                    }
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Even elements from array: ");
+                foreach (var element in array)
+                {
+                    if (element % 2 == 0)
+                    {
+                        Console.Write(element + " ");
+                    }
+                }
+                Console.WriteLine();
             }
             catch (Exception ex)
             {
